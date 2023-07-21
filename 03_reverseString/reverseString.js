675761b11869c77f041f675a7a3b8f6e6f9a5210
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-07-21 17:48:19 +0900

    Reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    reverseword = '';
 
-};
+        for (let i = word.length -1; i >= 0; i--) {
+            reverseword += word.charAt(i);
+
+        }
+        
+        return reverseword;
+
+    };
 
 // Do not edit below this line
 module.exports = reverseString;
