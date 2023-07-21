32c0a2fc396f585b7630073d5860d137351ca3a2
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-07-21 14:34:13 +0900

    Concatenate string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function (s, n) {
+    if (n < 0) {
+        return "ERROR"
+    }
+    output = '';
+    for (let i = 0; i < n; i++) {
+        output += s;
+    }
+    return output;
 };
 
 // Do not edit below this line
