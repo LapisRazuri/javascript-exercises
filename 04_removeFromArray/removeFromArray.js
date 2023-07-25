bb3d3fb68675c94e7bc1ad94bf1a468947fd31df
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-07-25 19:31:11 +0900

    Fail remove multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function ( array, element, anotherelement ) {
+        for ( let i = 0; i <= array.length; i++ ) {
+            if ( array[i] == element || array[i] == anotherelement ) {
+                array.splice( i , 1 );
+            }
+        }
+        return array;
 };
 
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 // Do not edit below this line
 module.exports = removeFromArray;
