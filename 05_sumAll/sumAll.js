b6921eadfa767f307f229bd2f300a08eca5877f4
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-07-28 13:55:27 +0900

    Complete sumAll assignmet

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(startnum, endnum) {
+    let result = 0;
+    if ( typeof(startnum) != "number" || typeof(endnum) != "number") {
+        return "ERROR";
+    }
 
-};
+    else if ( startnum < 0 || endnum < 0 ) {
+        return "ERROR";
+    } 
 
+    else if ( startnum < endnum ) {
+        for (let i = startnum; i <= endnum; i++ ) {
+        
+            result += i; 
+        }
+    }
+
+    else if ( startnum > endnum ) {
+        for (let i = startnum; i >= endnum; i-- ) {
+        
+            result += i; 
+        }
+    }
+
+
+    return result;
+
+}
 // Do not edit below this line
 module.exports = sumAll;
