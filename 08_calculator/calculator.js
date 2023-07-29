ad9b75980019f9bc86135c9cf8e178a08f0f44fd
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-07-29 17:32:33 +0900

    Complete task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,54 @@
-const add = function() {
+const add = function( num1, num2 ) {
+  return num1 + num2 ;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2 ;
 	
 };
 
-const sum = function() {
+const sum = function( array ) {
+  let result = 0;
+
+  for (let i = 0; i < array.length; i++ ) {
+    result += array[i];    
+
+  }
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function( ...args ) {
+  let result = 1;
+
+  for (let i = 0; i < args.length; i++ ) {
+    result *= args[i];
+
+  }
+  return result;
 
 };
 
-const power = function() {
+const power = function( num, power ) {
+  let result = 1;
+
+  for (let i = 0; i < power; i++ ) {
+    result *= num;
+    
+  }
+  return result;
 	
 };
 
-const factorial = function() {
+const factorial = function( num ) {
+  let result = 1;
+  for (let i = 1; i <= num; i++ ) {
+    result *= i;
+
+  }
+
+  return result;
 	
 };
 
