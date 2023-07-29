1b8063919e244f4b346b58e0222ffec0e50598e9
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-07-29 14:58:04 +0900

    Complete task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,38 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let result = ( temp - 32 ) / 1.8;
+
+
+    if (result == .00) {
+      Number(result);
+      return result;
+    }
+
+
+    else {
+      let roundedresult = Number(result.toFixed(1));
+      return roundedresult;
+
+    }
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = temp * 1.8 + 32 ;
+  
+  if (result == .00) {
+    Number(result);
+    return result;
+  }
+
+
+  else {
+    let roundedresult = Number(result.toFixed(1));
+    return roundedresult;
+
+  }
+  
 };
+
 
 // Do not edit below this line
 module.exports = {
