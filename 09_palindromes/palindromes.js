10ecbabb9dcaf554ab829728fe5613f1ca0476e3
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-08-01 10:40:20 +0900

    Complete tast. Left wrong code as comments as for future reference

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,41 @@
-const palindromes = function () {
+const palindromes = function ( word ) { 
+    //let result = word.replace(/[^a-z]/gi, "")
+    
+    
+        let result = word.match(/[a-z0-9]/gi)
+        console.log(result)
+         
+            //for (let i = 0; i < word.length; i++ ) {
+            //if ( word.charAt(i) != [i] ) {continue};   
+            //result += word.charAt(i);
+            //}
 
-};
+     
+        result = String(result).toLowerCase();
+        console.log(result);              
+        let reverseword = ""; 
+
+            for (let i = result.length -1; i >= 0; i--) {
+                //if ( word.charAt(i) != word.match(/[a-z]/i) ) {continue}; 
+                reverseword += result.charAt(i); 
+
+                  
+            }
+
+            
+
+        if ( result === reverseword ) {
+            return true;
+            }
+              
+        else {
+            return false;
+            }
+               
+      }         
+                
+        
+
 
 // Do not edit below this line
 module.exports = palindromes;
