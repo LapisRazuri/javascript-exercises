cd0f2481c66cd782b30acf793de209dfc9b620ca
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-08-03 11:08:17 +0900

    Complete task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const getTheTitles = function( books ) {
+    const array = [];
 
-};
+    for ( let i = 0; i < books.length; i++ ) {
+        for ( var key in books[i] ) {
+                array[i] = books[i].title;
+            
+
+        
+            //console.log(books[i][key]);
+
+        }           
+    }
+
+    return array;
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
